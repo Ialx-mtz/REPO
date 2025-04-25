d912c27f48f5e04aa0ca6dd7f0b78172fb2471de
Ialx-mtz <00053325@uca.edu.sv>
2025-04-24 20:52:29 -0600

    Exercise 3 IF

---
+++ Ejercicio3If.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number1, number2;
+
+    cout << "Please enter the first number " << endl;
+    cin >> number1;
+
+    cout << "Please enter the second number " << endl;
+    cin >> number2;
+
+    return 0;
+}
