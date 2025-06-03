9214b78dcc11fdc570b710a0dbd6b650446ffc2d
Ialx-mtz <00053325@uca.edu.sv>
2025-06-03 09:58:50 -0600

    update :memo:

--- E1.cpp
+++ E1.cpp
@@ -2,4 +2,35 @@
 
 using namespace std;
 
-int main();
+int main(){
+    int kw;
+
+    cout << "Ingresa tu consumo de kWh para brindarte tu tarifa " << endl;
+    cin >> kw;
+
+    if (kw >= 0 && kw <= 100)
+    {
+        cout << "Tu tarifa a pagar es $50.00 dolares" << endl;
+
+    } else if (kw >= 101 && kw <= 200)
+    {
+        cout << "Tu tarifa a pagar es $125.00 dolares" << endl;
+
+    } else if (kw > 200)
+    {
+        cout << "Tu tarifa a pagar es $275.00 dolares" << endl;
+
+    } else
+    {
+        cout << "Por favor ingresa una cantidad de consumo valida" << endl;
+    }
+    
+    
+    
+    
+
+
+    return 0;
+}
+
+
